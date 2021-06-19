7b80275bdd1ec75ae83317db43876c0bdaa822eb
Collin Chapman <cchap14@gmail.com>
2021-06-19 16:57:40 -0700

    add delete route and view for Treat, add Add/Delete Flavor route and view for Treat

--- SweetAndSavory/Controllers/TreatsController.cs
+++ SweetAndSavory/Controllers/TreatsController.cs
@@ -78,6 +78,50 @@ namespace SweetAndSavory.Controllers
       return RedirectToAction("Details", new { id = treat.TreatId });
     }
 
+    public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorTreat");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+
+    public ActionResult AddFlavor(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        if (_db.FlavorTreat.Any(join => join.FlavorId == FlavorId && join.TreatId == treat.TreatId) == false)
+          _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = treat.TreatId });
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      _db.Treats.Remove(thisTreat);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+
+    public ActionResult DeleteFlavor(int joinId)
+    {
+      var joinEntry = _db.FlavorTreat.FirstOrDefault(entry => entry.FlavorTreatId == joinId);
+      _db.FlavorTreat.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.TreatId });
+    }
+
 
 
 
