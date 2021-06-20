a630249d9acc1bd142de8773d911ecb387481edf
Collin Chapman <cchap14@gmail.com>
2021-06-19 17:19:33 -0700

    add Details route and view to Flavors

--- SweetAndSavory/Controllers/FlavorsController.cs
+++ SweetAndSavory/Controllers/FlavorsController.cs
@@ -52,5 +52,15 @@ namespace SweetAndSavory.Controllers
       return RedirectToAction("Index");
     }
 
+    [AllowAnonymous]
+    public ActionResult Details(int id)
+    {
+      var thisFlavor = _db.Flavors
+      .Include(flavor => flavor.JoinEntities)
+      .ThenInclude(join => join.Treat)
+      .FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
   }
 }
